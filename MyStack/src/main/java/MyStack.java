3425f2792a6b364499e7b43666cbe8d58ced38d2
Vladislav Gaar <gaarvlad@gmail.com>
2025-01-22 19:49:58 +0300

    Added MyStack class template

--- MyStack/src/main/java/MyStack.java
+++ MyStack/src/main/java/MyStack.java
@@ -1,5 +1,68 @@
-public class MyStack {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.Iterator;
+import java.util.List;
+
+public class MyStack<T> implements Iterable<T> {
+    private final List<T> elements;
+
+    public MyStack() {
+        this.elements = new ArrayList<>();
     }
+
+    /**
+     * Добавляет элемент в верх стека.
+     */
+    public void push(T item) {}
+
+    /**
+     * Извлекает верхний элемент стека.
+     * @return верхний элемент
+     * @throws EmptyStackException если стек пуст
+     */
+    public T pop() {}
+
+    /**
+     * Возвращает верхний элемент стека, не удаляя его.
+     * @return верхний элемент
+     * @throws EmptyStackException если стек пуст
+     */
+    public T peek() {}
+
+    /**
+     * Проверяет, пуст ли стек.
+     */
+    public boolean isEmpty() {}
+
+    /**
+     * Возвращает текущий размер стека.
+     */
+    public int size() {}
+
+    /**
+     * Проверяет, содержится ли элемент в стеке.
+     */
+    public boolean contains(T item) {}
+
+    /**
+     * Для соответствия требованиям:
+     * Аналог push (добавляем элемент в стек).
+     */
+    public boolean add(T item) {}
+
+    /**
+     * Удаляет из стека первый встретившийся элемент.
+     */
+    public boolean remove(Object o) {}
+
+    /**
+     * Возвращает итератор по элементам стека (от первого добавленного к последнему).
+     */
+    @Override
+    public Iterator<T> iterator() {}
+
+    /**
+     * Дополнительный метод, если нужно получить элемент по индексу.
+     */
+    public T get(int index) {}
 }
